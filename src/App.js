366702b599b1a09c5ceb7e65f7548032af652ed9
janellebaguley <nellie.baguley@gmail.com>
2021-05-07 07:38:27 -0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './component/Header/Header'
+import routes from './routes'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      {routes}
     </div>
   );
 }
---
+++ src/component/Contact/Contact.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Contact.css';
+
+const Contact = props => {
+    let [firstName, setFirstName] = useState(''),
+        [lastName, setLastName] = useState(''),
+        [email, setEmail] = useState(''),
+        [subject, setSubject] = useState(''),
+        [message, setMessage] = useState(''),
+        [messageSent, setMessageSent] = useState(false);
+
+
+    useEffect(() => {
+        document.title = 'Contact - Michelle'
+    }, [])
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        axios.post('/api/email', {firstName, lastName, email, subject, message})
+        .then(() => {
+            setMessageSent(true);
+            setSubject('');
+            setMessage('');
+        })
+        .catch(err => console.log(err))
+    }
+    return(
+        <main className='contact-me'>
+            <section className='contact-info'>
+                {/* <p>Contact</p> */}
+            </section>
+            {!messageSent
+                ? (
+                    <form className='contact-form'>
+                        <label>Name *</label>
+                        <div>
+                            <section className='name-input'>
+                                <input value={firstName} 
+                                autoComplete='given-name' required onChange={e => setFirstName(e.target.value)}/>
+                                <span className ='name'>First Name</span>
+                            </section>
+                            <section className='name-input'>
+                                <input value={lastName}
+                                autoComplete='given-name' 
+                                required onChange={e => setLastName(e.target.value)}/>
+                                <span className ='name'>Last Name</span>
+                            </section>
+                        </div>
+                        <label>Email *</label>
+                        <input value={email} required onChange={e => setEmail(e.target.value)}/>
+                        <label>Subject *</label>
+                        <input value={subject} required onChange={e => setSubject(e.target.value)}/>
+                        <label className= 'message'>Message *</label>
+                        <textarea value={message} required autoComplete = 'off' onChange={e => setMessage(e.target.value)}/>
+                        <button className= 'submit-button' onClick={sendMessage}>Submit</button>
+                    </form>
+                ) 
+                : (
+                    <section>
+                        <h2>Your message was sent.</h2>
+                        <h3>I will reply to your message as soon as possible.</h3>
+                        <button onClick={() => setMessageSent(false)}>New Message</button>
+                    </section>
+                )}
+               
+        </main>
+    )
+}
+export default Contact;
