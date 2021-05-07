031a97c3aeba83bf7370a6c29eb6a34b6ed31ee8
janellebaguley <nellie.baguley@gmail.com>
2021-05-07 08:44:58 -0600

    server and db updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
-const ctrl = require('./controllers/mailController'),
-    //   blogCtrl = require('./controllers/blogController')
+const express = require('express'),
+      ctrl = require('./controllers/mailController'),
+      blogCtrl = require('./controllers/blogController')
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const app = express()
+app.use(express.json())
 
 //nodemailer endpoint
 app.post('/api/mail', ctrl.email)
+
+//blog
+app.get('/api/blog/:id', blogCtrl.getPosts)
 
 app.listen(SERVER_PORT, () => console.log(`Running on port: ${SERVER_PORT}`))
