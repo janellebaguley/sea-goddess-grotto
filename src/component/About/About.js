79d75ea8b33cc1eadae3fc301b4dece4102c9b00
janellebaguley <nellie.baguley@gmail.com>
2021-05-30 08:39:00 -0600

    general updates

---
+++ src/component/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About (){
+ return(
+     <div>
+         <section className='about'>
+         <h1>hello</h1>
+         </section>
+     </div>
+ )
+}
+export default About;
--- src/component/Psychic/Psychic.js
+++ src/component/Psychic/Psychic.js
@@ -1,2 +1,8 @@
 import React from 'react';
 
+function Psychic (){
+ return(
+     <div className = 'psychic'><h1>hello</h1></div>
+ )
+}
+export default Psychic;
