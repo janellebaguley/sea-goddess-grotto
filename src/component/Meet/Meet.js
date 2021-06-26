e976b5dc2ec1ce0ebd65d9926174dc65f275d1c6
janellebaguley <nellie.baguley@gmail.com>
2021-06-26 14:13:09 -0600

    shop and meetupdates

--- src/component/Meet/Meet.js
+++ src/component/Meet/Meet.js
@@ -5,6 +5,21 @@ function Meet (){
     return(
         <div className='meet'>
             <h1>Meet the Mermaids</h1>
+            <br/>
+            <h2>Calypso Mahina:</h2>
+            <img/>
+            <br/>
+            <p>
+            Calypso was born in the beautiful tide pools in La Jolla in 1888. She adores the entire coastline of Southern California, but currently calls the beaches of Del Mar home. Calypso met her wife, Luna, while exploring a remote beach. Together Calypso and Luna decided to follow their dream and live on land among the humans. Although she grew up in the wild Pacific waters and returns home frequently, she adores living among humans and experiencing their unique lifestyles, whimsical homes, and modern comforts. She is able to transform from her fins to feet as she wishes, but even in her human form she chooses to always adorn herself with reminders of her ocean home. Calypso loves children and seeing the joy on their faces when they get to meet a real mermaid. She also can be a bit of a salty siren with the adults and loves to tease. Calypso loves singing and entertaining humans of all ages and backgrounds. She also loves using some of her mermaid magic to let others feel the joy of being a mermaid as well. During her off time Calypso can be found exploring her San Diego home, travelling, and investigating new humans and their strange technology and creations.
+            </p>
+            <br/>
+            <br/>
+            <h2>Luna the Sea Nymph</h2>
+            <img/>
+            <br/>
+            <p>
+            Luna was born in 1891 on Miraflores Beach in Lima, Peru. 
+            </p>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import Blog from './component/Blog/Blog'
 import Contact from './component/Contact/Contact'
 // import Psychic from './component/Psychic/Psychic'
 import Shop from './component/Shop/Shop'
-import MyCalendar from './component/Events/MyCalendar'
+import Events from './component/Events/Events'
 import Meet from './component/Meet/Meet'
 import FAQ from './component/FAQ/Faq'
 import Documents from './component/Docs/Documents'
@@ -19,7 +19,7 @@ export default (
         <Route path= '/contact' component={Contact}/>
         {/* <Route path= '/psychic' component={Psychic}/> */}
         <Route path= '/MER-chandise' component={Shop}/>
-        <Route path= '/performancesandevents' component={MyCalendar}/>
+        <Route path= '/performancesandevents' component={Events}/>
         <Route path= '/meetthemermaids' component={Meet}/>
         <Route path= '/faq' component={FAQ}/>
         <Route path= '/documents' component={Documents}/>
