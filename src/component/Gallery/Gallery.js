1bbf284cd6c57418e604e7423697fb6e68a726ae
janellebaguley <nellie.baguley@gmail.com>
2021-07-03 11:16:01 -0700

    updated images and package.json

--- src/component/Gallery/Gallery.js
+++ src/component/Gallery/Gallery.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import './Gallery.css'
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import "react-image-gallery/styles/css/image-gallery.css";
+import Gallery from "react-image-gallery";
 
-function Gallery (){
-    return(
-        <div className='meet'>
-            <h1>Gallery</h1>
-        </div>
-    )
+const Gallery = () => {
+  const [images, setImages] = useState(null)  
+  
+  useEffect(() => {
+    let shouldCancel = false    
+    const call = async () => {
+      const response = await axios.get(
+        // 'https://'
+      );      if (!shouldCancel && response.data && response.data.length > 0) {
+        setImages(response.data.map(url => ({
+          original: `${url}=w1024`,
+          thumbnail: `${url}=w100`
+        })))
+      }
+    }
+    call()
+    return () => shouldCancel = true
+  }, [])  
+  
+  return images ? <Gallery items={images} /> : null
 }
-export default Gallery;
+export default Gallery
+
+// https://medium.com/@ValentinHervieu/how-i-used-google-photos-to-host-my-website-pictures-gallery-d49f037c8e3c
--- src/component/Patron/Patron.js
+++ src/component/Patron/Patron.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Patreon from '../assets/patreon.png'
 import './Patron.css'
 
 export default function Patron(){
@@ -10,7 +11,7 @@ export default function Patron(){
             Want to experience some mermaid magic, but maybe don’t have a need for a large scale event or a party? Come be a Patron! On our patreon page we offer several different tiers to fit your budget and needs. In some tiers you will have access to our free public events calendar (we do at least 2 a month!), tutorials and patterns on how we make and alter our costuming, Q&A sessions, video chats, mermaid oracle readings, and so much more!
             </p>
             <br/>
-            <img/>
+            <img src={Patreon} alt='Patreon Link' />
         </div>
     )
 }
