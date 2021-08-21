e173a6e41ca0690355b91ba34890b88be92b7f40
janellebaguley <nellie.baguley@gmail.com>
2021-08-21 17:07:55 -0600

    updates to mobile styling

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
       {routes}
       <footer className='footer'  >
                     <nav className='nav-links-f'>
-                        <Link to ='/' className="nav-links-f nowrap">Home</Link>
+                        <Link to ='/' className="nav-links-f">Home</Link>
                         <Link to = '/performancesandevents' className="nav-links-f nowrap">Performances and Events</Link>
                         <Link to = '/MER-chandise' className="nav-links-f nowrap">MER-chandise</Link>
-                        <Link to = '/meetthemermaids' className="nav-links-f nowrap wrap">Meet the Mermaids</Link>
+                        <Link to = '/meetthemermaids' className="nav-links-f  nowrap corner">Meet the Mermaids</Link>
                         {/* <Link to='/patron' className='nav-links-f'>Patron</Link> */}
                         {/* <Link to = '/gallery' className='nav-links-f'>Gallery</Link> */}
                         {/* <Link to = '/blog' className='nav-links-f'>Blog</Link> */}
-                        <Link to = '/contact' className='nav-links-f'>Contact</Link>
+                        <Link to = '/contact' className='nav-links-f '>Contact</Link>
                         <Link to = '/faq' className='nav-links-f'>FAQ</Link>
                         {/* <Link to = '/documents' className='nav-links-f'>Documents</Link> */}
                         {/* <Link to = '/policies' className='nav-links-f'>Policies</Link> */}
